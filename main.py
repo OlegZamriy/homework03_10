b2eccf4c1f1e53b86952afea3915dcd4dc7ca5e0
lysenko_v <lysenko_v@itstep.academy>
2023-10-03 19:15:26 +0300

    1

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-def display_even_numbers(start, end):
-    for num in range(start, end + 1):
-        if num % 2 == 0:
-            print(num)
+def display_square(side_length, fill, filled=True):
+    for i in range(side_length):
+        if filled:
+            row = fill * side_length
+        else:
+            if i == 0 or i == side_length - 1:
+                row = fill * side_length
+            else:
+                row = fill + " " * (side_length - 2) + fill
+        print(row)
 
-start = int(input("Введіть початкове число: "))
-end = int(input("Введіть кінцеве число: "))
+print("Заповнений квадрат:")
+display_square(5, "$", True)
 
-display_even_numbers(start, end)
+print("Порожній квадрат:")
+display_square(5, "$", False)
