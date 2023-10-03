538896c454e1ed543b16649f10fb6e155dd9386e
lysenko_v <lysenko_v@itstep.academy>
2023-10-03 19:07:07 +0300

    1

---
+++ main.py
@@ -0,0 +1,7 @@
+def display_quote(author):
+    quote = "Don't compare yourself with anyone in this world…\nif you do so, you are insulting yourself."
+    formatted_quote = f'"{quote}"\n{author}'
+    print(formatted_quote)
+
+author = "Bill Gates"
+display_quote(author)
